221859e48e67b6f2ae9699bd1bfa233634dcad82
AlexeyShumeyko <schumeiko.alexei@yandex.by>
2024-06-17 16:36:47 +0300

    Create an abstraction of shapes and classes

---
+++ GeometryLibrary/Shape/Circle.cs
@@ -0,0 +1,19 @@
+﻿using GeometryLibrary.Interfaces;
+
+namespace GeometryLibrary.Shape
+{
+    public class Circle : IShape
+    {
+        public double Radius { get; private set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public double CalculateArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+}
---
+++ GeometryLibrary/Shape/Triangle.cs
@@ -0,0 +1,38 @@
+﻿using GeometryLibrary.Interfaces;
+
+namespace GeometryLibrary.Shape
+{
+    public class Triangle : IShape
+    {
+        public double SideA { get; private set; }
+        public double SideB { get; private set; }
+        public double SideC { get; private set; }
+
+        public Triangle(double sideA, double sideB, double sideC)
+        {
+            SideA = sideA;
+            SideB = sideB;
+            SideC = sideC;
+        }
+
+        public double CalculateArea()
+        {
+            double semiPerimeter = (SideA + SideB + SideC) / 2;
+
+            var perimeter = Math.Sqrt(semiPerimeter
+                * (semiPerimeter - SideA)
+                * (semiPerimeter - SideB)
+                * (semiPerimeter - SideC));
+
+            return perimeter;
+        }
+
+        public bool IsRightAngled()
+        {
+            double[] sides = { SideA, SideB, SideC };
+            Array.Sort(sides);
+
+            return Math.Pow(sides[0], 2) + Math.Pow(sides[1], 2) == Math.Pow(sides[2], 2);
+        }
+    }
+}
